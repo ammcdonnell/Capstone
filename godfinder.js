8de922d6518c3aba66baf7708bc65d9b79773b89
Francisco Perozo <fperozo@lclark.edu>
2021-06-30 00:15:05 -0700

    functioning translator with limited information and without formatting

--- godfinder.js
+++ godfinder.js
@@ -2,20 +2,41 @@ function checkForTranslation()
 {
   var input = document.getElementById("inputTextField").value;
   var outputDiv = document.getElementById("translationOutputDiv");
+  var selectedMyth = document.getElementById("mythSelector").value;
   input = input.toLowerCase();
-  if (input == "love")
+  if (selectedMyth == "Greek") //all greek gods
   {
-    outputDiv.innerHTML = "Aphrodite";
+    if (input == "love")
+    {
+      outputDiv.innerHTML = "Aphrodite";
+    }
+    //For synonyms or other translations:
+    else if (input == "beauty")
+    {
+      outputDiv.innerHTML = "Aphrodite";
+    }
+    //At the end of each translation, to avoid wrong output if random input is added
+    else
+    {
+      outputDiv.innerHTML = "";
+    }
   }
-  //For synonyms or other translations:
-  else if (input == "beauty")
+  if (selectedMyth == "Japanese") //all Japanese gods
   {
-    outputDiv.innerHTML = "Aphrodite";
-  }
-  //At the end of each translation, to avoid wrong output if random input is added
-  else
-  {
-    outputDiv.innerHTML = "";
+    if (input == "fox")
+    {
+      outputDiv.innerHTML = "Inari";
+    }
+    //For synonyms or other translations:
+    else if (input == "thunder")
+    {
+      outputDiv.innerHTML = "Fujin";
+    }
+    //At the end of each translation, to avoid wrong output if random input is added
+    else
+    {
+      outputDiv.innerHTML = "";
+    }
   }
 
 }
